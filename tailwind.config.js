4354d3daba1b9282d7ddfa48bfd9194723ce080a
Naoki0509 <70335387+Naoki0509@users.noreply.github.com>
2022-03-13 22:00:06 +0900

    📦 パッケージの追加

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	content: ["src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			padding: {
+				container: "0.2rem",
+				main: "4rem 0",
+				footer: "2rem 0",
+			},
+		},
+	},
+	plugins: [],
+};
